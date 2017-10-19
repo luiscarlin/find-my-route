8d9ef7b773c7a1f4d5e1cc3d6102c5f82f107bd9
Luis Carlin <luiscarlin@gmail.com>
2017-10-19 07:37:46 -0400

    Add inital express server

--- server/app.js
+++ server/app.js
@@ -1,1 +1,11 @@
-import path from 'path'
+import express from 'express'
+
+const app = express()
+
+app.get('*', (req, res) => {
+  res.send('hello from the other side')
+})
+
+const port = 3000
+
+app.listen(port, () => console.log(`Running on port ${port}`))
