82674dea2ccd10b1d33875ea34393e2ce1ce6828
Luis Carlin <luiscarlin@gmail.com>
2017-10-19 07:42:12 -0400

    Add nodemon to restart server when files change

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 
 app.get('*', (req, res) => {
-  res.send('hello from the other side')
+  res.send('hello from the other side!')
 })
 
 const port = 3000
