763fcea2a3f3c35b043a6683bf90e4b591f18ca6
Luis Carlin <luiscarlin@gmail.com>
2017-10-20 07:58:00 -0400

    Return status

--- server/app.js
+++ server/app.js
@@ -4,7 +4,8 @@ import bodyParser from 'body-parser'
 
 const app = express()
 app.use(bodyParser.json())
-app.post('api/controller', controller.handlePost)
+
+app.post('/api/controller', controller.handlePost)
 
 const port = 3000
 app.listen(port, () => console.log(`Running on port ${port}`))
--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,5 +1,6 @@
 const controller = {}
 
 controller.handlePost = (req, res, next) => {
+  res.sendStatus(200)
 }
 export default controller
